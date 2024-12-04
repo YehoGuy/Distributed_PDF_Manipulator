6cd4e90c1cbb7e283b02822102d1ece799fe5ebd
YehoGuy <yehowork23@gmail.com>
2024-12-04 13:56:47 +0200

    add initial generateHtmlFile method to the worker

--- project/src/main/java/Worker/Main.java
+++ project/src/main/java/Worker/Main.java
@@ -1,4 +1,9 @@
 package Worker;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class Main {
@@ -27,6 +32,23 @@ public class Main {
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private static void generateHtmlFile(String outputFilePath, List<String> results) throws Exception {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+            writer.write("<html><body>");
+            writer.write("<h1>PDF Processing Results</h1>");
+            writer.write("<ul>"); //unordered list
+            for (String result : results) {
+                writer.write("<li>" + result + "</li>"); //list item
+            }
+            writer.write("</ul>");
+            writer.write("</body></html>");
+
+            System.out.println("Output HTML file generated: " + outputFilePath);
+        } catch (IOException e) {
+            throw new Exception("Worker Error generating HTML file: " + e.getMessage());
         }
     }
 }
